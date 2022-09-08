25c40c411c14e5ec7c683acf448453f0193e8823
bbyya551 <abe109yd576@gmail.com>
2022-09-08 21:21:54 +0900

    first commit

---
+++ src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name1;
+		name1 = "abe";
+		System.out.println(name1);
+		
+		name1 = "apple";
+		System.out.println(name1);
+		
+		final int LUCKY_NUMBER = 10;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
